d12fbec908e7a462c1ae1061fe3f1b7c00ecf46f
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-02-17 08:19:02 -0600

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Matthew RIley on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
