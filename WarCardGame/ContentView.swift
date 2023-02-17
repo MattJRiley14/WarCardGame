dfcca517e0794c399945e70e4488edbefb451c0e
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-02-17 09:01:41 -0600

    Created all content/elements for game.

--- WarCardGame/ContentView.swift
+++ WarCardGame/ContentView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("War Card Game")
+        ZStack {
+            Image("background")
+            
+            VStack {
+                Image("logo")
+                
+                HStack {
+                    Image("card5")
+                    Image("card5")
+                }
+                
+                Button {
+                    print("Button clicked!")
+                } label: {
+                    Image("dealbutton")
+                }
+                
+                HStack {
+                    VStack {
+                        Text("Player")
+                        Text("0")
+                    }
+                    VStack {
+                        Text("CPU")
+                        Text("0")
+                    }
+                }
+            }
         }
     }
 }
